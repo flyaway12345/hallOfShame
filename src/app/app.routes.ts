c22499fe178f1b8e54fd869acaa3213adabe786b
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2024-04-09 17:40:34 -0400

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HallofshameComponent } from './hallofshame/hallofshame.component';
-import { Error404Component } from './error404/error404.component';
-
-export const routes: Routes = [
-    {path: '' ,component: HallofshameComponent},
-    {path: '**' ,component: Error404Component}
-];
